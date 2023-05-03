ed1d30b9bfe9d7e6c69ac3d00ecd5d257fd1f4ac
LlucSerramitjana <lluc.serramitjana@estudiantat.upc.edu>
2023-05-03 09:55:27 +0200

    generar claus working

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const { generateMyRsaKeys } = require('./index');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
 // Generem les claus RSA
 app.get('/generate-rsa-keys', async (req, res) => {
   console.log('Generating RSA keys...');
@@ -13,8 +20,53 @@ app.get('/generate-rsa-keys', async (req, res) => {
   console.log('Public key:', publicKey);
   console.log('Private key:', privateKey);
   res.json({
+    publicKey: {
+      e: publicKey.e.toString(),
+      n: publicKey.n.toString()
+    },
+    privateKey: {
+      d: privateKey.d.toString(),
+      n: privateKey.n.toString()
+    }
+  });
+});
+//encriptar
+app.post('/encrypt', async (req, res) => {
+  const { message, key } = req.body || {};
+
+  if (!message || !key) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  const publicKey = new MyRsaPublicKey(BigInt(key.e), BigInt(key.n));
+  const encrypted = publicKey.encrypt(BigInt(message));
+  res.json({ encrypted: encrypted.toString() });
+});
+//decrypt
+app.post('/decrypt', async (req, res) => {
+  const { ciphertext, key } = req.body;
+  const privateKey = new MyRsaPrivateKey(BigInt(key.d), BigInt(key.n));
+  const decrypted = privateKey.decrypt(BigInt(ciphertext));
+  res.json({ decrypted: decrypted.toString() });
+});
+//sign
+app.post('/sign', async (req, res) => {
+  const { message, key } = req.body;
+  const privateKey = new MyRsaPrivateKey(BigInt(key.d), BigInt(key.n));
+  const signature = privateKey.sign(BigInt(message));
+  res.json({ signature: signature.toString() });
+});
+//verify
+app.get('/verify', async (req, res) => {
+  const message = BigInt(req.query.message);
+  const signature = BigInt(req.query.signature);
+  const publicKey = new MyRsaPublicKey(BigInt(req.query.e), BigInt(req.query.n));
+  const verified = publicKey.verify(signature);
+  res.json({
+    message: message.toString(),
+    signature: signature.toString(),
     publicKey: publicKey.toString(),
-    privateKey: privateKey.toString()
+    verified: (verified === message)
   });
 });
 app.listen(port, () => {
